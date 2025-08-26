f02314bc56a315ef4a96941b78f0cd3b8b6d16b1
SCfmk <66367779+SCfmk@users.noreply.github.com>
2025-08-26 07:24:46 +0200

    Create test.ts

---
+++ test.ts
@@ -0,0 +1,18 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.SmallSquare)
+    if (rfid2.isCardPresent()) {
+        const uid = rfid2.readUidHex()
+        if (uid.length > 0) {
+            basic.showString(uid)
+        } else {
+            basic.showString("No UID")
+        }
+    } else {
+        basic.showString("No card")
+    }
+    basic.clearScreen()
+})
+
+basic.pause(100)
+rfid2.init(0x28)
+basic.showString("RFID2")
